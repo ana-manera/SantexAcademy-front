29cbbc6391e353ac11a965abaab4de3ee3780b54
dgauna <danicarpsv@gmail.com>
2023-08-11 15:43:21 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
 
 const routes: Routes = [
+  { path: 'login', component:LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  {path: 'recovery', component: PasswordRecoveryComponent},
+  { path: 'home', component: HomeComponent},
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
   },
   {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: 'login'
   }
 ];
 
