8196270ba5f0fb847655b8a3b24ebbc759ca0ba2
Ana Victoria Manera <anavictoriamanera@gmail.com>
2023-10-17 21:23:07 -0300

    fix router login/register

--- src/app/modules/login/register/register.component.ts
+++ src/app/modules/login/register/register.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  constructor(
+    public authenticationService: AuthService,
+    private router: Router,
+    private activated: ActivatedRoute
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
